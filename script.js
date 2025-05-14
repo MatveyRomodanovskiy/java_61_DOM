c838f861d02a4a0cf199ee8e6d3075c0cbd10e2f
matveyromodanovskiy <startpolus@gmail.com>
2025-05-14 17:13:26 +0300

    step 5

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
+
+
 // Ждём, когда всё загрузится
 document.addEventListener('DOMContentLoaded', () => {
     const input = document.getElementById('taskInput');       // поле ввода
     const list = document.getElementById('taskList');         // список задач
     const button = document.getElementById('addButton');      // кнопка
 
-    // Навешиваем обработчик клика на кнопку
-    button.addEventListener('click', () => {
+    function addTask() {
         const taskText = input.value.trim(); // убираем лишние пробелы
 
         if (taskText !== '') {
@@ -14,18 +15,15 @@ document.addEventListener('DOMContentLoaded', () => {
             list.appendChild(li);                    // добавляем в список
             input.value = '';                        // очищаем поле
         }
+    }
+    // Навешиваем обработчик клика на кнопку
+    button.addEventListener('click', () => {
+        addTask();
     });
 
     input.addEventListener('keydown', (event) => {
         if (event.key === 'Enter') {
-            const taskText = input.value.trim(); // убираем лишние пробелы
-
-            if (taskText !== '') {
-                const li = document.createElement('li'); // создаём <li>
-                li.textContent = taskText;               // вставляем текст
-                list.appendChild(li);                    // добавляем в список
-                input.value = '';                        // очищаем поле
-            }
+            addTask()
         }
     });
 });
